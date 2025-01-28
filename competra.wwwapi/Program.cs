871ec479b7e3a4084792feee087b31454fc806ad
Ludvig Mathias Sti <69594206+LudvigSti@users.noreply.github.com>
2025-01-28 12:46:14 +0100

    vi prøver

--- competra.wwwapi/Program.cs
+++ competra.wwwapi/Program.cs
@@ -4,6 +4,7 @@ using competra.wwwapi.Repositories.Interfaces;
 using competra.wwwapi.Repositories.Repos;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://*:80");
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
